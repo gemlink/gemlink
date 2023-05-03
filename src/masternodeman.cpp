66371b336a5167a814eda164a1d9a2f7a84d0f6d
Txid <tinh.mrlov3@gmail.com>
2023-05-03 16:28:51 +0000

    feat: define MN_WINNER_MINIMUM_AGE_TESTNET

--- src/masternodeman.cpp
+++ src/masternodeman.cpp
@@ -20,8 +20,8 @@
 #include <boost/filesystem.hpp>
 #include <boost/thread.hpp>
 
-#define MN_WINNER_MINIMUM_AGE 8000     // Age in seconds. This should be > MASTERNODE_REMOVAL_SECONDS to avoid misconfigured new nodes in the list.
-#define MN_WINNER_MINIMUM_AGE_TEST 200 // Age in seconds. This should be > MASTERNODE_REMOVAL_SECONDS to avoid misconfigured new nodes in the list.
+#define MN_WINNER_MINIMUM_AGE 8000        // Age in seconds. This should be > MASTERNODE_REMOVAL_SECONDS to avoid misconfigured new nodes in the list.
+#define MN_WINNER_MINIMUM_AGE_TESTNET 200 // Age in seconds. This should be > MASTERNODE_REMOVAL_SECONDS to avoid misconfigured new nodes in the list.
 
 /** Masternode manager */
 CMasternodeMan mnodeman;
