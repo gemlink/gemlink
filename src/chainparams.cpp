d7866c826ad6eb2de4d48935dd81c51954b941df
Txid <tinh.mrlov3@gmail.com>
2026-02-27 16:41:03 +0000

    feat: update testnet fork for xavier

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -125,6 +125,8 @@ public:
         consensus.vUpgrades[Consensus::UPGRADE_KRAKOA].nProtocolVersion = 170012;
         consensus.vUpgrades[Consensus::UPGRADE_LATVERION].nActivationHeight = 3730000;
         consensus.vUpgrades[Consensus::UPGRADE_LATVERION].nProtocolVersion = 170012;
+        consensus.vUpgrades[Consensus::UPGRADE_XAVIER].nActivationHeight = 4275000; // 2024, Mar 29
+        consensus.vUpgrades[Consensus::UPGRADE_XAVIER].nProtocolVersion = 170012;
 
         consensus.nZawyLWMA3AveragingWindow = 60;
         // The best chain should have at least this much work.
@@ -474,6 +476,12 @@ public:
         txid.SetHex("041931e512daf53ca0852e01baa4a3aae6d72783422adb921fb53e3ec19b395d");
         vWhitelistTx[COutPoint(txid, (uint32_t)1)] = (COutPoint(txid, (uint32_t)1));
 
+        txid.SetHex("559e52339471724e2bcebe9c827bce116e07f4ae3bebe675ad2758e30cb09b15");
+        vWhitelistTx[COutPoint(txid, (uint32_t)1)] = (COutPoint(txid, (uint32_t)1));
+
+        txid.SetHex("559e52339471724e2bcebe9c827bce116e07f4ae3bebe675ad2758e30cb09b15");
+        vWhitelistTx[COutPoint(txid, (uint32_t)1)] = (COutPoint(txid, (uint32_t)0));
+
         nPoolMaxTransactions = 3;
         strSporkKey = "045da9271f5d9df405d9e83c7c7e62e9c831cc85c51ffaa6b515c4f9c845dec4bf256460003f26ba9d394a17cb57e6759fe231eca75b801c20bccd19cbe4b7942d";
 
@@ -543,8 +551,10 @@ public:
         consensus.vUpgrades[Consensus::UPGRADE_LATVERIA].nProtocolVersion = 170012;
         consensus.vUpgrades[Consensus::UPGRADE_KRAKOA].nActivationHeight = 81600; // 2023, Jun 06
         consensus.vUpgrades[Consensus::UPGRADE_KRAKOA].nProtocolVersion = 170012;
-        consensus.vUpgrades[Consensus::UPGRADE_LATVERION].nActivationHeight = 85600; // 2024, Oct 12
+        consensus.vUpgrades[Consensus::UPGRADE_LATVERION].nActivationHeight = 81900; // 2024, Oct 12
         consensus.vUpgrades[Consensus::UPGRADE_LATVERION].nProtocolVersion = 170012;
+        consensus.vUpgrades[Consensus::UPGRADE_XAVIER].nActivationHeight = 82000; // 2024, Oct 12
+        consensus.vUpgrades[Consensus::UPGRADE_XAVIER].nProtocolVersion = 170012;
         consensus.nMasternodePaymentsStartBlock = 1500;
         consensus.nMasternodePaymentsIncreasePeriod = 200;
         consensus.nZawyLWMA3AveragingWindow = 60;
@@ -951,6 +961,10 @@ bool CChainParams::GetCoinbaseProtected(int height) const
 
 int CChainParams::GetmnLockBlocks(int height) const
 {
+    if (consensus.NetworkUpgradeActive(height, Consensus::UPGRADE_XAVIER)) {
+        return 0;
+    }
+
     if (!consensus.NetworkUpgradeActive(height, Consensus::UPGRADE_LATVERION)) {
         return mnLockBlocks;
     } else {
